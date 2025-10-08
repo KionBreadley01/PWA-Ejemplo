453a12914229a75ac7eba0c1134f9f913177a25d
KionBreadley01 <juansanchezjuan125@gmail.com>
2025-10-08 12:58:22 -0600

    correciones 3

--- ejemplo 1/sw.js
+++ ejemplo 1/sw.js
@@ -33,7 +33,8 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(
+                () => caches.match(`${BASE_PATH}/offline.html`));
         })
     );
 });
